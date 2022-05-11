ab142c82d724cf0c68aec905f05472a19a29b6d0
Ilia Byzov <17595195+vuode@users.noreply.github.com>
2022-05-11 19:57:14 +0200

    Add display of loading status

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,14 +4,17 @@ import Card from '../Card';
 
 import { useLaunchesQuery } from '../../generated';
 
+import styles from './App.module.css';
+
 const limit = 10;
 
 const App: React.FC = () => {
-  const { data, fetchMore } = useLaunchesQuery({
+  const { data, loading, fetchMore } = useLaunchesQuery({
     variables: {
       limit,
       offset: 0,
     },
+    notifyOnNetworkStatusChange: true,
   });
 
   const [currentOffset, setCurrentOffset] = useState(limit);
@@ -28,7 +31,9 @@ const App: React.FC = () => {
       {entries &&
         entries.map((entry) => entry && <Card key={entry.id} entry={entry} />)}
 
-      {total > currentOffset && (
+      {loading && <div className={styles.loading}>Loading...</div>}
+
+      {!loading && total > currentOffset && (
         <button
           type='button'
           onClick={() => {
